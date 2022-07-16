a49055154cc827d7fd060f9572b1b8271915a06c
daic0r <daiceman@gmx.net>
2022-07-16 14:13:33 +0200

    Use Google benchmark as test

--- main.cpp
+++ main.cpp
@@ -1,23 +1,32 @@
 #include "thread_pool.h"
 #include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <benchmark/benchmark.h>
 
-int main() {
-   thread_pool pool{};
+constexpr auto NUM_TASKS = 10000;
+std::vector<int> vNums(1000);
 
-   std::vector<std::future<int>> v;
-   v.reserve(1000);
-
-   std::cout << "Pushing threads..." << std::endl;
-   for (int i = 0; i < 1000; ++i)
-      v.push_back(pool.async([i]() { return i; }));
-
-   std::cout << "Waiting for futures..." << std::endl;
-
-   for (auto& f : v) {
-      f.wait();
-      std::cout << f.get() << " ";
-   }
-
-   std::cout << "\n\nDone\n";
-
+static void DoSetup(const benchmark::State& state) {
+   std::iota(vNums.begin(), vNums.end(), 0);
+   std::cout << "Setup...\n";
 }
+
+static void BM_Accumulate(benchmark::State& state) {
+   thread_pool pool(state.range(0));
+   std::cout << "Running with " << state.range(0) << " queues.\n";
+   std::vector<std::future<int>> v;
+   v.reserve(NUM_TASKS);
+   for (auto _ : state) {
+      for (int i = 0; i < NUM_TASKS; ++i)
+         v.push_back(pool.async([]() { return std::accumulate(vNums.begin(), vNums.end(), 0); }));
+
+      for (auto& f : v) {
+         f.wait();
+      }
+   }
+}
+
+BENCHMARK(BM_Accumulate)->Threads(1)->Arg(1)->Arg(8)->Setup(DoSetup); //; 
+
+BENCHMARK_MAIN();
